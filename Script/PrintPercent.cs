603d081935ff9994af93c8b07e06c699bf693162
Codingindark <rochakrupakheti20@gmail.com>
2021-05-02 23:15:15 +0545

    Initial scene and c# script is created

---
+++ Script/PrintPercent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintPercent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        ///Declaration of constants and variables
+
+        const int MaxScore = 100;
+        int Score = 83;
+        float Percent = (float)Score / MaxScore;
+
+        ///Output
+        print("Percentage : " + Percent + "%");
+	}
+	
+	
+}
